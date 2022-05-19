9c69759d735cdfa7b3688b284a55c1c34acbf887
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-19 04:22:52 +0200

    basic routing concepts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    ddd($id);
+    return response('Post '.$id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request) {
+    dd($request->name);
 });
