ada1d25f92b17989813e9791876aa712d82f1291
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-22 11:49:14 +0200

    fix route model binding

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/', function () {
 // Single Listing
 Route::get('/listings/{listing}', function(Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
 
