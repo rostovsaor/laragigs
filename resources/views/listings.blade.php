ae48d6dcbbfae2e21bd26696e2a99476af9cf806
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-22 09:30:11 +0200

    basic blade layout directives

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,3 +1,6 @@
+@extends('layout')
+
+@section('content')
 <h1>{{$heading}}</h1>
 <!-- @if(count($listings) == 0)
   No listings found
@@ -19,3 +22,5 @@
 @else
   <p>No listings found</p>
 @endunless
+
+@endsection
